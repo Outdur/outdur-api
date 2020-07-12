6bdb1f58fe784bd3d1d975a83b7c68be9b43aef7
Chibuzo <chibuzo.henry@gmail.com>
2020-07-12 10:27:17 +0100

    Remove redundant code

--- tests/user/user.test.ts
+++ tests/user/user.test.ts
@@ -1,6 +1,4 @@
-import 'mocha';
 import chai, { expect } from 'chai';
-import { userRouter } from "../../src/user/userRouter";
 import chaiHttp from 'chai-http';
 chai.use(chaiHttp);
 const server = require('../../index');
@@ -9,7 +7,6 @@ describe('User API', () => {
     describe('POST user/', () => {
         it('should fail when no contact field is included', async() => {
             const res = await chai.request(server).post('/users');
-            console.log(res.body)
         });
     });
 });
