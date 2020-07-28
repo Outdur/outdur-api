43c2e9c6b5fc3f894ffa182a60ce2d48ed4b63b5
Austin <augustineumeagudosi@gmail.com>
2020-07-29 00:59:45 +0100

    tryinig to save invite to db

--- src/invite/inviteService.ts
+++ src/invite/inviteService.ts
@@ -1,68 +1,81 @@
-import { IUser } from "../user/userInterface";
-import { IUsers } from "../user/usersInterface";
+import { IInvite } from "./inviteInterface";
+import { IInvites } from "./invitesInterface";
 import { isEmail, isLength, isNumeric } from "../helpers/validator";
 import { inviteModel } from './inviteModel';
 import { handleError } from "../helpers/handleError";
+import { generateCode } from "../helpers/generateCode";
 
 
-const create = async (userData: any) => {
-    const validationError = validateNewUser(userData);
+const create = async (inviteData: any) => {
+    const validationError = validateInvite(inviteData);
     if (validationError) throw new handleError(422, validationError);
+   
+    //generate invite code
+    const code: number = generateCode(3.1536e+10, 9.1536e+10);
+  
+    const userContact = isNumeric(inviteData.contact) ? { phone: inviteData.contact } : { email: inviteData.contact };
+    let circle_id, event_id;
 
-    const userContact = isNumeric(userData.contact) ? { phone: userData.contact } : { email: userData.contact };
+    if (inviteData.circle_id) { circle_id = inviteData.circle_id };
+    if (inviteData.event_id) { event_id = inviteData.event_id };
+    
+    const user = inviteData.user; // I need to fetch this data from the token
+    
+    const invitePayload: any = [code, userContact, circle_id, event_id, user];
+    
     try {
-        return await inviteModel.create(userContact);
+        return await inviteModel.create(invitePayload);
     } catch (err) {
         let message = err.code == '11000' ? 'Duplicate contact' : 'Unknown fatal error occurred';
         throw new handleError(500, message);
     }
 }
 
-const findOne = async (user_id: number): Promise<IUser | any> => {
-    const user =  await inviteModel.findOne({ user_id });
-    if (!user) throw new handleError(404, 'User not found');
-    return user;
-}
+// const findOne = async (user_id: number): Promise<IInvite | any> => {
+//     const user =  await inviteModel.findOne({ user_id });
+//     if (!user) throw new handleError(404, 'User not found');
+//     return user;
+// }
 
-const findAll = async (): Promise<IUsers | any[]> => {
-    return await inviteModel.find();
-}
+// const findAll = async (): Promise<IInvites | any[]> => {
+//     return await inviteModel.find();
+// }
 
-const update = async (userData: IUser): Promise<IUser | any> => {
-    const validationError = validateUpdateUser(userData);
-    if (validationError) throw new handleError(422, validationError);
+// const update = async (inviteData: IInvite): Promise<IInvite | any> => {
+//     const validationError = validateUpdateUser(inviteData);
+//     if (validationError) throw new handleError(422, validationError);
     
-    const user_id = userData.user_id;
-    delete userData.user_id;
-    const updatedUser = await inviteModel.findOneAndUpdate({ user_id }, userData, { new: true });
-    return updatedUser;
-}
+//     const user_id = inviteData.user_id;
+//     delete inviteData.user_id;
+//     const updatedUser = await inviteModel.findOneAndUpdate({ user_id }, inviteData, { new: true });
+//     return updatedUser;
+// }
 
-const validateNewUser = (user: any): null | string => {
-    if (!user.contact) return 'Contact field must not be empty';
-    if (isNumeric(user.contact)) {
-        if (!isLength(user.contact, { min: 6, max: 15 })) return 'Contact phone must not be less than 6 or greater than 15 numbers';
+const validateInvite = (invite: any): null | string => {
+    if (!invite.contact) return 'Contact field must not be empty';
+    if (isNumeric(invite.contact)) {
+        if (!isLength(invite.contact, { min: 6, max: 15 })) return 'Contact phone must not be less than 6 or greater than 15 numbers';
     } else {
-        if (!isEmail(user.contact)) return 'Contact email is invalid';
+        if (!isEmail(invite.contact)) return 'Contact email is invalid';
     }
     return null;
 }
 
-const validateUpdateUser = (user: any): null | string => {
-    Object.keys(user).forEach(key => {
-        if (!['email', 'phone', 'firstname', 'lastname', 'user_id'].includes(key)) throw new Error(`The field ${key} is not allowed`);
-    });
+// const validateUpdateUser = (user: any): null | string => {
+//     Object.keys(user).forEach(key => {
+//         if (!['email', 'phone', 'firstname', 'lastname', 'user_id'].includes(key)) throw new Error(`The field ${key} is not allowed`);
+//     });
     
-    if (user.phone && isNumeric(user.phone)) {
-        if (!isLength(user.phone, { min: 6, max: 15 })) return 'Phone must not be less than 6 or greater than 15 numbers';
-    } else if (user.phone && !isNumeric(user.phone)) return 'Phone must be numeric';
-    if (user.email && !isEmail(user.phone)) return 'Contact email is invalid';
-    return null;
-}
+//     if (user.phone && isNumeric(user.phone)) {
+//         if (!isLength(user.phone, { min: 6, max: 15 })) return 'Phone must not be less than 6 or greater than 15 numbers';
+//     } else if (user.phone && !isNumeric(user.phone)) return 'Phone must be numeric';
+//     if (user.email && !isEmail(user.phone)) return 'Contact email is invalid';
+//     return null;
+// }
 
 module.exports = {
     create,
-    findOne,
-    findAll,
-    update
+    // findOne,
+    // findAll,
+    // update
 }
