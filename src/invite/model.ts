8b8e52f992f028e29e43f35925ca7233f5f59a71
Chibuzo <chibuzo.henry@gmail.com>
2020-08-02 17:24:17 +0100

    Change model schema definition

--- src/invite/model.ts
+++ src/invite/model.ts
@@ -4,36 +4,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const inviteSchema = new Schema({
-    email: {
-        type: String, trim: true, index: {
-            partialFilterExpression: { email: { $type: "string" } }
-        }
-    },
-    phone: {
-        type: String, trim: true, index: {
-            partialFilterExpression: { phone: { $type: "string" } }
-        }
-    },
-    user: {
-        type: String, trim: true, index: {
-            partialFilterExpression: { user: { $type: "string" } }
-        }
-    },
-    event_id: {
-        type: String, trim: true, index: {
-            partialFilterExpression: { event_id: { $type: "string" } }
-        }
-    },
-    circle_id: {
-        type: String, trim: true, index: {
-            partialFilterExpression: { circle_id: { $type: "string" } }
-        }
-    },
-    code:  {
-        type: String, trim: true, index: {
-            partialFilterExpression: { circle_id: { $type: "string" } }
-        }
-    },
+    email: { type: String, trim: true, index: true },
+    phone: { type: String, trim: true, index: true },
+    user: { type: String, trim: true, index: true },
+    event_id: { type: String, trim: true, index: true },
+    circle_id: { type: String, trim: true, index: true },
+    code:  { type: String, trim: true, index: true, unique: true },
     status: String
 }, { timestamps: true });
 
